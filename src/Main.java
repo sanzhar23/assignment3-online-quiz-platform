5c4af7eeccb52b2d71fa34bf2da850c302e05913
Ss <252808@astanait.edu.kz>
2026-01-23 21:11:59 +0500

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import model.EasyQuiz;
 import model.Quiz;
 import model.Question;
 import repository.QuizRepository;
@@ -8,35 +7,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //quiz
         QuizRepository quizRepo = new QuizRepository();
-
-        //создание нового quiz
-        Quiz quiz = new EasyQuiz("Test Quiz " + System.currentTimeMillis());
-        quizRepo.create(quiz);
+        QuestionRepository questionRepo = new QuestionRepository();
 
         System.out.println("=== ALL QUIZZES ===");
         for (Quiz q : quizRepo.getAll()) {
             System.out.println(q.getId() + " | " + q.label());
         }
 
-        //берем quiz 1
+        if (quizRepo.getAll().isEmpty()) {
+            System.out.println("No quizzes found.");
+            return;
+        }
+
         int quizId = quizRepo.getAll().get(0).getId();
 
         System.out.println("\n=== QUIZ BY ID ===");
         System.out.println(quizRepo.getById(quizId).label());
 
-        // вопросы
-        QuestionRepository questionRepo = new QuestionRepository();
-
-        String text = "2 + 2 = ?";
-        String answer = "4";
-
-        if (!questionRepo.existsSameQuestion(quizId, text, answer)) {
-            Question question = new Question(quizId, text, answer, 1);
-            questionRepo.create(question);
-        }
-        System.out.println("\n===QUESTIONS FOR QUIZ " + quizId + "===");
+        System.out.println("\n=== QUESTIONS FOR QUIZ " + quizId + " ===");
         for (Question q : questionRepo.getAllByQuizId(quizId)) {
             System.out.println(
                     q.getId() + " | " +
